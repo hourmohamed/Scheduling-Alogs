74141bd02930a002df005207b72c6a733f302b64
hour <hour.mohamed01@gmail.com>
2024-12-28 19:24:41 +0200

    HRRN trace tested

--- HRRN.cpp
+++ HRRN.cpp
@@ -20,6 +20,7 @@ void HRRN::schedule(std::vector<Process>& processes) {
     std::vector<bool> completed(processes.size(), false);
 
     while (completed_count < processes.size()) {
+    
         int max_index = -1;
         double max_ratio = -1.0;
 
@@ -40,31 +41,37 @@ void HRRN::schedule(std::vector<Process>& processes) {
             Process& selected_process = processes[max_index];
             current_time = std::max(current_time, selected_process.arrivalTime);
 
-            // Mark the process as running by updating its state
-            for (int t = current_time; t < current_time + selected_process.serviceTime; ++t) {
-                if (t >= selected_process.state.size()) {
-                    selected_process.state.resize(t + 1, 0);
-                }
-                selected_process.state[t] = 1; // Process is running
+            
+            
+            for (int t = selected_process.arrivalTime; t < current_time; ++t) {
+                int wrapped_time = t % this->time_line;  
+                selected_process.state[wrapped_time] = 0;  
             }
 
-            // Update process timings
+            
+            current_time = std::max(current_time, selected_process.arrivalTime);
+
+          
+            for (int t = current_time; t < current_time + selected_process.serviceTime; ++t) {
+                int wrapped_time = t % this->time_line;  
+                selected_process.state[wrapped_time] = 1; 
+            }
+
+            // std::cerr << "heeeeeeeeeeeerrrrrrrrrrrrrrrrrrrrreeeeeeeeeeeeeeeeeeee" << std::endl;
             selected_process.finishTime = current_time + selected_process.serviceTime;
             selected_process.turnAroundTime = selected_process.finishTime - selected_process.arrivalTime;
-            std::cerr << "Turnaround for: " << selected_process.name << " is " << selected_process.turnAroundTime << std::endl;
+            // std::cerr << "Turnaround for: " << selected_process.name << " is " << selected_process.turnAroundTime << std::endl;
 
+            
             current_time = selected_process.finishTime;
             completed[max_index] = true;
             completed_count++;
         } else {
-            // If no process is ready, increment time
+            
             std::cout << "No process ready; incrementing time to " << (current_time + 1) << std::endl;
             current_time++;
         }
     }
 
-    // Debug: Print turnaround times
-    for (int i = 0; i < processes.size(); i++) {
-        std::cerr << "Turnaround times: " << processes[i].turnAroundTime << " " << processes[i].name << std::endl;
-    }
+   
 }
